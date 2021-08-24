7b84de37262a22be1cba044635944446b594d334
Nurbolot <kgznurik17@gmail.com>
2021-08-24 19:07:55 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import React from 'react';
 import './App.css';
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './styles/Navbar.css'
 
 function App() {
   return (
     <div className="App">
-      hghfhgf
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
+
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Logo from '../assets/pizzaLogo.png';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <div>
-            <h1>This is a navigation bar</h1>
+        <div className="navbar">
+            <div className="leftSide">
+                <div className="image">
+                    <img src={Logo} alt="logo" />
+                </div>
+            </div>
+            <div className="rightSide">
+                <Link to='/'>home </Link>
+                <Link to='/about'>about </Link>
+                <Link to='/menu'>menu </Link>
+                <Link to='/contact'>contact </Link>
+            </div>
         </div>
     )
 }
