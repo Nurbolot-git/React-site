32c24a2ee509ad86d398816966b1acf0fa965d7e
Nurbolot <kgznurik17@gmail.com>
2021-08-25 19:36:26 +0300

    added navbar's mobile hide show function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/pizzaLogo.png';
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
+
+    const [openLinks, setOpenLinks] = useState(false)
+
+    const NavToggle = () => {
+        setOpenLinks(!openLinks)
+    }
     return (
         <div className="navbar">
-            <div className="leftSide">
+            <div className="leftSide" id={openLinks ? "open" : "close"}>
                 <div className="image">
                     <img src={Logo} alt="logo" />
-                    <div className="hiddenLinks">
+                    <div className="hiddenLinks" >
                         <Link to='/'>home </Link>
                         <Link to='/about'>about </Link>
                         <Link to='/menu'>menu </Link>
@@ -22,7 +28,7 @@ function Navbar() {
                 <Link to='/about'>about </Link>
                 <Link to='/menu'>menu </Link>
                 <Link to='/contact'>contact </Link>
-                <button>
+                <button onClick={NavToggle}>
                     <ReorderIcon />
                 </button>
             </div>
