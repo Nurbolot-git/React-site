611145e26c80e900cc4da65f75295efd6fc81ede
Nurbolot <kgznurik17@gmail.com>
2021-08-28 21:08:51 +0300

    added home styles and home tags

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './styles/Navbar.css'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
         </Switch>
+        <Footer />
       </Router>
 
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import BannerImage from '../assets/pizza.jpeg';
+import '../styles/Home.css';
 function Home() {
-    return <div>Loremsit</div>
+    return (
 
+        <section className="home">
+            <div className="headerContainer"
+                style={{ backgroundImage: `url(${BannerImage})` }}>
+                <h1 className="header__title">
+                    title
+                </h1>
+                <p className="header__text">
+                    lorem ipsum dolor sit amet, consectetur adip
+                </p>
+                <Link to="/menu">
+                    < button >order now</button>
+                </Link>
+            </div>
+        </section >
+
+    )
 }
 
 export default Home
